f73f43fc4a947ff875a75c3e4131469441e66ed5
nizarabdouss <nizarabdouss@gmail.com>
2023-05-23 00:37:54 -0400

    Navbar partially done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,9 +26,9 @@ const Navbar = () => {
             <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
                 {["Market", "Exchange", "Wallets", "Tutorials"].map((item, index) => (
                     <NavbarItem  key={item+index} title={item}/>
-                ))};
+                ))}
                 <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-                    Login
+                    Connect Wallet
                 </li>
             </ul>
             <div className="flex relative">
@@ -37,10 +37,16 @@ const Navbar = () => {
                 :    <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>
                 }
                 {toggleMenu && (
-                    <ul>
+                    <ul
+                        className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-one 
+                        flex flex-col justify start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+                    >
                         <li className="text-xl w-full my-2">
-                            <AiOutlineClose onClick={() => }}
+                            <AiOutlineClose onClick={() => setToggleMenu(false)} />
                         </li>
+                        {["Market", "Exchange", "Wallets", "Tutorials"].map((item, index) => (
+                        <NavbarItem  key={item+index} title={item} classProps="my-2 text-lg"/>
+                ))};
                     </ul>
                 )}
             </div>
