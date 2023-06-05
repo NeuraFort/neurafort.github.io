ae81b026e1579d481ac2983e94222536728d2d5f
nizarabdouss <nizarabdouss@gmail.com>
2023-06-05 01:36:23 -0400

    dist

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: './src/main.jsx',
   output: {
-    path: path.resolve(__dirname, './'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   module: {
