e2cdf49befaa94accbe6d5d74423fa3857a45f94
nizarabdouss <nizarabdouss@gmail.com>
2023-06-05 03:16:33 -0400

    back to jsx point to bundle.js now

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: "./src/main.js",
+  entry: "./src/main.jsx",
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
