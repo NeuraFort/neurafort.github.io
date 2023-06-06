1b24bd7f1f00c8a188cdaa51321b56d384bf27e5
nizarabdouss <nizarabdouss@gmail.com>
2023-06-05 22:48:38 -0400

    changed navbar, searchbar and welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,7 @@
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from 'react-icons/si';
 import{ BsInfoCircle } from 'react-icons/bs';
+import { IoMdOpen } from "react-icons/io";
 
 
 const Welcome = () => {
@@ -15,10 +16,10 @@ const Welcome = () => {
             <div className="flex md:flex-row flex-col items-strart justify-between md:p-20 py-12 px-4 ">
                 <div className="flex flex-1 justify-start flex-col md:mr-10 ">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1 ">
-                        Send Crypto <br /> across the world 
+                        Welcome to <br /> NeuraFort
                     </h1>
                     <p className ="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-                        Explore the crypto world.
+                        Generate an authenticity report
                     </p>
                     <button 
                         type="button"
@@ -26,8 +27,9 @@ const Welcome = () => {
                         className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#3546eb] "
                     >
                         <p className="text-white text-base font-semibold ">
-                            Connect Wallet
+                            Start Now
                         </p>
+                        <IoMdOpen/>
                     </button>
                     <div className="grid sm:grid-cols-3 grid-cols-2  w-full mt-10">
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
@@ -46,30 +48,40 @@ const Welcome = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10 ">
-                    <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+                <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+                    <div className="p-3 justify-end items-start flex-col rounded-xl w-72 h-96 my-5 document-card white-glassmorphism">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
                                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                                    <SiEthereum fontSize={21} color="#fff">
-
-                                    </SiEthereum>
+                                <SiEthereum fontSize={21} color="#fff" />
                                 </div>
-                                <BsInfoCircle fontSize={17} color="#fff"></BsInfoCircle>
+                                <BsInfoCircle fontSize={17} color="#fff" />
                             </div>
                             <div>
-                                <p className="text-white font-light text-sm">
-                                    Address
-
-                                </p>
-                                <p className="text-white font-semibold text-lg mt-1 ">
-                                    Ethereum
-                                </p>
+                                <p className="text-white font-light text-sm">Report ID</p>
+                                <p className="text-white font-semibold text-lg mt-1">Sample Report</p>
+                            </div>
+                            <div className="mt-4">
+                                <p className="text-gray-300 font-light text-xs"style={{ fontSize: '16px' }}>Date</p>
+                            </div>
+                            <div className="mt-4">
+                                <p className="text-gray-300 font-light text-xs"style={{ fontSize: '16px' }} >Authors</p>
+                            </div>
+                            <div className="mt-4">
+                                <p className="text-gray-300 font-light" style={{ fontSize: '16px' }} >Content</p>
+                            </div>
+                            <div className="mt-4">
+                                <p className="text-gray-300 font-light" style={{ fontSize: '16px' }} >Certificate</p>
+                            </div>
+                            <div className="mt-4">
+                                <p className="text-gray-300 font-light" style={{ fontSize: '16px' }} >Signature</p>
                             </div>
                         </div>
+
                     </div>
-                    <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism"></div>
+                    <div className="p-5 w-96 h-96 flex flex-col justify-start items-center blue-glassmorphism"></div>
                 </div>
+
             </div>
         </div>
     );
