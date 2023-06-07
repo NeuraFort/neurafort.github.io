79f0fc77189ea16bba293161c15979b74de3d4e5
nizarabdouss <nizarabdouss@gmail.com>
2023-06-07 16:48:37 -0400

    added authentication page and connections

--- client/src/components/RegisterProduct.jsx
+++ client/src/components/RegisterProduct.jsx
@@ -1,7 +1,131 @@
+import{ useState } from 'react';
+
 const RegisterProduct = () => {
-    return (
-        <h1>RegisterProduct</h1>
-    );
-}
+  const [productName, setProductName] = useState('');
+  const [productDescription, setProductDescription] = useState('');
+  const [images, setImages] = useState([]);
+
+  const handleProductNameChange = (e) => {
+    setProductName(e.target.value);
+  };
+
+  const handleProductDescriptionChange = (e) => {
+    setProductDescription(e.target.value);
+  };
+
+  const handleImageUpload = (e) => {
+    const uploadedImages = Array.from(e.target.files);
+    setImages(uploadedImages);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform product registration logic here
+    // You can access the product name, description, and images from the state
+    // and send them to the server or perform any other necessary actions
+    console.log('Product Name:', productName);
+    console.log('Product Description:', productDescription);
+    console.log('Images:', images);
+  };
+
+  return (
+    <div className="container mx-auto py-10">
+      <div className="max-w-3xl mx-auto text-center">
+        <h1 className="text-4xl font-bold mb-4 text-gradient">Authenticate with NeuraFort</h1>
+        <form onSubmit={handleSubmit}>
+            <div className="blue-glassmorphism">
+                <div className="mb-6">
+                    <label htmlFor="productName" className="block text-lg font-bold text-gradient mb-2">Manufacturer & Model</label>
+                    <input
+                    type="text"
+                    id="productName"
+                    value={productName}
+                    onChange={handleProductNameChange}
+                    className="w-full rounded-3xl border-gray-300 px-3 py-2 focus:outline-none focus:border-blue-500"
+                    placeholder="Enter the manufacturer and model of the item"
+                    required
+                    />
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="serialNumber" className="block text-lg font-bold text-gradient mb-2">Serial Number</label>
+                    <input
+                    type="text"
+                    id="serialNumber"
+                    value={productName}
+                    onChange={handleProductNameChange}
+                    className="w-full rounded-3xl border-gray-300 px-3 py-2 focus:outline-none focus:border-blue-500"
+                    placeholder="Enter the serial number of the item"
+                    required
+                    />
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="size" className="block text-lg font-bold text-gradient mb-2">Size</label>
+                    <input
+                    type="text"
+                    id="size"
+                    value={productName}
+                    onChange={handleProductNameChange}
+                    className="w-full rounded-3xl border-gray-300 px-3 py-2 focus:outline-none focus:border-blue-500"
+                    placeholder="Enter the size of the item"
+                    required
+                    />
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="year" className="block text-lg font-bold text-gradient mb-2">Year</label>
+                    <input
+                    type="text"
+                    id="year"
+                    value={productName}
+                    onChange={handleProductNameChange}
+                    className="w-full rounded-3xl border-gray-300 px-3 py-2 focus:outline-none focus:border-blue-500"
+                    placeholder="Enter the year the item was produced/released"
+                    required
+                    />
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="productDescription" className="block text-lg font-bold text-gradient mb-2">Additional information (e.g., Color, Condition, item history)</label>
+                    <textarea
+                    id="productDescription"
+                    value={productDescription}
+                    onChange={handleProductDescriptionChange}
+                    className="w-full rounded-3xl border-gray-300 px-3 py-2 focus:outline-none focus:border-blue-500"
+                    required
+                    ></textarea>
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="imageUpload" className="block text-lg font-bold text-gradient mb-2">Upload images of your item, supporting documents</label>
+                    <input
+                    type="file"
+                    id="imageUpload"
+                    onChange={handleImageUpload}
+                    className="mb-2 rounded-3xl"
+                    multiple
+                    accept="image/*"
+                    required
+                    />
+                    {images.length > 0 && (
+                    <div>
+                        <h3>Selected Images:</h3>
+                        {images.map((image, index) => (
+                        <div key={index}>
+                            <img src={URL.createObjectURL(image)} alt={`Image ${index + 1}`} className="w-20 h-20 object-cover mr-2" />
+                        </div>
+                        ))}
+                    </div>
+                    )}
+                </div>
+        
+            </div>  
+          
+          <div className="mt-10">
+            <button type="submit" className=" bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">Register Product</button>
+          </div>
+          
+          
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default RegisterProduct;
